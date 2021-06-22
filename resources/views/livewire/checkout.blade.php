1a6bf7ae6d07abe82fec071896d91ad4e3ee2f9b
yogesh-karki <yogesh@hueshine.com>
2021-06-22 16:23:57 +0545

    [UPDATE] checkout

--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -1,7 +1,133 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+
 <div class="main">
     <div class="main-content">
 
         <div class="checkout-box">
+            
+            <div class="transaction-box">
+                <div class="tb-info">
+                    <h4 class="transaction-tb-head">Personal Information</h4>
+                    
+                    <div class="choose-mem">
+                        <label class="cm-card">
+                            Member
+                            <input type="radio" checked="checked" name="personal-info">
+                            <span class="checkmark"></span>
+                        </label>
+
+                        <label class="cm-card">
+                            Non Member
+                            <input type="radio"  name="personal-info">
+                            <span class="checkmark"></span>
+                        </label>
+
+                    </div>
+
+                    <div class="cart-entries">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="entry">
+                                    <label >Full Name</label>
+                                    <input type="text">
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="entry">
+                                    <label >Phone</label>
+                                    <input type="number">
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="entry">
+                                    <label >Email</label>
+                                    <input type="email">
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="entry">
+                                    <label >Address</label>
+                                    <input type="text">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="tb-info">
+                    <h4 class="transaction-tb-head">Promotion & Discounts</h4>
+
+                    <div class="cart-entries">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="entry">
+                                    <label >Promotion</label>
+                                    <input type="number">
+                                </div>
+                            </div>
+
+                            <div class="col-md-3">
+                                <div class="entry">
+                                    <label >Discount Amount</label>
+                                    <input type="number">
+                                </div>
+                            </div>
+
+                            <div class="col-md-3">
+                                <div class="entry">
+                                    <label >Gift Card</label>
+                                    <input type="number">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="tb-info">
+                    <h4 class="transaction-tb-head">Note</h4>
+
+                    <div class="cart-entries">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="entry">
+                                    <label >Description</label>
+                                    <textarea></textarea>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="tb-info payment-method">
+                    <h4 class="transaction-tb-head">Payment Method</h4>
+
+                    <div class="choose-mem">
+                        <label class="cm-card">
+                            Cash 
+                            <input type="radio" checked="checked" name="payment-method">
+                            <span class="checkmark"></span>
+                        </label>
+
+                        <label class="cm-card">
+                            Card
+                            <input type="radio"  name="payment-method">
+                            <span class="checkmark"></span>
+                        </label>
+
+                    </div>
+
+                </div>
+
+  
+            </div>
+
             <div class="cart-wrap">
                 <div class="checkout-head">
                     <h4>Service Cart</h4>
@@ -40,27 +166,40 @@
             
                 </div>
 
-                <div class="cart-total">
-                    <h6>Total</h6>
-                    <h4 class="cart-total-price">$170</h4>
 
-                </div>
 
-            </div>
+                <div class='cart-amount'>
 
-            <div class="transaction-box">
-                <div class="tb-info">
-                    <h4 class="transaction-tb-head">Personal Information</h4>
                     
-                    <div class="choose-mem">
-                        <a href="#" class="service-user is-member active">Member</a>
-                        <a href="#" class="service-user no-member">Non Member</a>
 
+                    <div class="cart-total">
+                        <h4 class="cart-total-price">$170</h4>
                     </div>
-                    
-                </div>
-            </div>
 
+                    <div class="discount">
+                        <h6>Senior Discount</h6>   
+
+                        <h5>$17</h5>
+                    </div>
+
+                    <div class="discount">
+                        <h6>Manual Discount</h6>
+                        <h5>$17</h5>
+                    </div>
+
+                    <div class="cart-grand-total">
+                        <h6>Grand Total</h6>
+                        <h4 class="cart-total-price">$150</h4>
+                    </div>
+                </div>
+
+                <div class="checkout">
+                    <a href="#" class="aside-btn">Proceed Transaction</a>
+                </div>
+
+                
+
+            </div>
         
         </div>
 
@@ -68,3 +207,12 @@
     
     </div>
 </div>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+<script>
+    $("#multiple").select2({
+          placeholder: "Select a programming language",
+          allowClear: true
+      });
+</script>
