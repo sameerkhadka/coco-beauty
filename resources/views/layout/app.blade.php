a80836cdb0af52a65f2bd8a8749ec8019b15df65
yogesh-karki <yogesh@hueshine.com>
2021-08-06 12:44:06 +0545

    fav icon

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,7 +8,7 @@
     <title>Admin | Coco Beauty</title>
 
 
-
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-icon.png') }}">
 
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
 
